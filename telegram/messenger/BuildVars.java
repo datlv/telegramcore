514c2f9a232df5ef3301c4b1949519d5a968f2a1
datle2011@gmail.com <datle2011@gmail.com>
2017-09-20 15:01:16 +0700

    init

---
+++ telegram/messenger/BuildVars.java
@@ -0,0 +1,40 @@
+/*
+ * This is the source code of Telegram for Android v. 3.x.x.
+ * It is licensed under GNU GPL v. 2 or later.
+ * You should have received a copy of the license in this archive (see LICENSE).
+ *
+ * Copyright Nikolai Kudashov, 2013-2017.
+ */
+
+package org.telegram.messenger;
+
+/*public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 1043;
+    public static String BUILD_VERSION_STRING = "4.2";
+    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
+    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20150326";
+}*/
+
+public class BuildVars {
+    public static boolean DEBUG_VERSION = false;
+    public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static int BUILD_VERSION = 1043;
+    public static String BUILD_VERSION_STRING = "4.2";
+    public static int APP_ID = 139942; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "43ab1ea1a7fa38501fec73c4e4ffea3d"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "3d7872d35b4343c688ee308b69ea5d37";
+    public static String HOCKEY_APP_HASH_DEBUG = "tdatlvwrappy";
+    public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+    public static String FOURSQUARE_API_KEY = "FMCWFJ1XCMZKDN4SGAWGCY34Y5IMRW3AZXGUHOOSCPEE2RTV"; //obtain your own KEY at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_ID = "JI022OC2C3BTTSLAXMSTZQWTQUYGV0MBNMICKEZTMCVD1LX2"; //obtain your own API_ID at https://developer.foursquare.com/
+    public static String FOURSQUARE_API_VERSION = "20161016";
+    public static String SEND_LOGS_EMAIL = "datle2011@gmail.com";
+}
